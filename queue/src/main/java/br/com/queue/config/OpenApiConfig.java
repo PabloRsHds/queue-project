291207c19e15a807f95cffbecfda6c8386d01b27
Pablo <pablorshds@gmail.com>
2026-09-17 22:25:07 -0300

    feat: adicionando classe de configuração do openApi

---
+++ queue/src/main/java/br/com/queue/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package br.com.queue.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Queue API",
+                version = "1.0.0",
+                description = "REST API para gerenciamento de filas"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
